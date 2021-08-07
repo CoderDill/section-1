54fb080c91d220c85268f3f4a8b6c65bd6127c4a
CrappyDill <mddillon47@gmail.com>
2021-08-07 08:21:35 -0700

    updated

--- jeopardy/jeopardy.js
+++ jeopardy/jeopardy.js
@@ -1,5 +1,7 @@
 // categories is the main data structure for the app; it looks like this:
-
+const $table = $("<table>").addClass("table");
+let $title = $("<h1>").text("Jeopardy!");
+let categories = [];
 //  [
 //    { title: "Math",
 //      clues: [
@@ -18,17 +20,6 @@
 //    ...
 //  ]
 
-let categories = [];
-const title = $("<h1>").prependTo("body").text("Jeopardy!");
-const btn = title.after(
-  $("<button>")
-    .text("Start!")
-    .on("click", function () {
-      getCategory();
-    })
-);
-const table = $("<table>");
-const categoriesRow = $("<thead>").append($("<tr>").text("Test"));
 /** Get NUM_CATEGORIES random category from API.
  *
  * Returns array of category ids
@@ -56,27 +47,57 @@ async function getCategoryIds() {
 
 async function getCategory(catId) {
   const response = await axios.get("https://jservice.io/api/category", {
-    params: { id: 4333 },
+    params: { id: catId },
   });
   console.log(response);
-  let ids = _.sampleSize(response.data, 6);
-  let clues = ids.map((clue) => ({
-    title: clue.category.title,
+  const title = response.data.title;
+  console.log(title);
+  const clues = response.data.clues;
+  let clueArray = clues.map((clue) => ({
     question: clue.question,
     answer: clue.answer,
     showing: null,
   }));
-  console.log(clues);
+  console.log({ title, clueArray });
+  return { title, clueArray };
 }
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
  *
  * - The <thead> should be filled w/a <tr>, and a <td> for each category
  * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
  *   each with a question for each category in a <td>
- *   (initally, just show a "?" where the question/answer would go.)
+ *   (intially, just show a "?" where the question/answer would go.)
  */
 
-async function fillTable() {}
+async function fillTable() {
+  $(".table").empty();
+  $("td").empty();
+  $("th").empty();
+  $("tr").empty();
+  const $thead = $("<thead>");
+  const $tbody = $("<tbody>");
+  const $tr = $("<tr>");
+  $("div").append($table);
+  $table.append($thead);
+  $thead.after($tbody);
+  $thead.append($tr);
+
+  for (let idx = 0; idx < 6; idx++) {
+    const $th = $("<th>").text(
+      JSON.stringify(categories[idx].title).toUpperCase()
+    );
+    $tr.append($th);
+  }
+  for (let idx1 = 0; idx1 < 5; idx1++) {
+    const $tr = $("<tr>");
+    $tbody.append($tr);
+    for (let idx2 = 0; idx2 < 6; idx2++) {
+      const $td = $("<td>").attr("id", `${idx2}-${idx1}`).text("?");
+      $td.click(handleClick);
+      $tr.append($td);
+    }
+  }
+}
 
 /** Handle clicking on a clue: show the question or answer.
  *
@@ -86,40 +107,56 @@ async function fillTable() {}
  * - if currently "answer", ignore click
  * */
 
-function handleClick(evt) {}
+function handleClick(evt) {
+  let id = evt.target.id;
+  let [catId, clueId] = id.split("-");
+  let clue = categories[catId].clueArray[clueId];
+  if ($(`#${catId}-${clueId}`).text() === "?") {
+    $(`#${catId}-${clueId}`).text(clue.question);
+  } else if ($(`#${catId}-${clueId}`).text() === clue.question) {
+    $(`#${catId}-${clueId}`).text(clue.answer);
+  }
+}
 
 /** Wipe the current Jeopardy board, show the loading spinner,
  * and update the button used to fetch data.
  */
 
-function showLoadingView() {}
-
 /** Remove the loading spinner and update the button used to fetch data. */
-
-function hideLoadingView() {}
-
+$("body").prepend(
+  $("<div>")
+    .prepend($title)
+    .append(
+      $("<button>")
+        .text("Start/Restart")
+        .on("click", function () {
+          return setupAndStart();
+        })
+    )
+);
 /** Start game:
  *
  * - get random category Ids
  * - get data for each category
  * - create HTML table
+ * - if err start-over
  * */
 
 async function setupAndStart() {
-  const catIds = await getCategoryIds();
-
-  categories = [];
-
-  for (let catId of catIds) {
-    categories.push(await getCategory(catId));
+  //spinner from loading.io
+  let spinner = $(
+    "<div class='lds-ring'><div></div><div></div><div></div><div></div></div>"
+  );
+  $("table").empty();
+  $("table").append(spinner);
+  try {
+    const catIds = await getCategoryIds();
+    categories = [];
+    for (let catId of catIds) {
+      categories.push(await getCategory(catId));
+    }
+    fillTable();
+  } catch (err) {
+    setupAndStart();
   }
-  fillTable();
 }
-
-/** On click of start / restart button, set up game. */
-
-// TODO
-
-/** On page load, add event handler for clicking clues */
-
-// TODO
